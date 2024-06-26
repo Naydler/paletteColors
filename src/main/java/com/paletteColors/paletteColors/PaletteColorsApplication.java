a111ffd089cbb6171e1b2e6b42230ec39e5794c0
Naydler <ilsalbesatrujillogerard@gmail.com>
2024-06-26 17:13:33 +0200

    first commit

---
+++ src/main/java/com/paletteColors/paletteColors/PaletteColorsApplication.java
@@ -0,0 +1,13 @@
+package com.paletteColors.paletteColors;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PaletteColorsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PaletteColorsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/paletteColors/paletteColors/PaletteColorsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.paletteColors.paletteColors;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PaletteColorsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
